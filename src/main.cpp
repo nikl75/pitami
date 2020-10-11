503483290ec2e8d2abd581305945f9d399c22a1c
nikl <dl@lahaye-tiedemann.de>
2020-10-11 17:23:26 +0200

    mit AccelStepper.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,50 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <Wire.h> // für LiquidCrystal
+#include <AccelStepper.h>
 
 // neues lcd initialisieren und pins setzen --------------------------------------------------------//
-LiquidCrystal mlcd(14, 15, 16, 17, 18 ,19);
+LiquidCrystal mlcd(14, 15, 16, 17, 18, 19);
 
 // pin stepper setzen
-int pStSt = 3;
-int pStDi = 4;
+#define STEPPER_01_ST 3
+#define STEPPER_01_DIR 4
+AccelStepper mStepper01(AccelStepper::DRIVER, STEPPER_01_ST, STEPPER_01_DIR);
 
-
-void setup() {
+void setup()
+{
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
   // lcd starten
   mlcd.clear();
-  mlcd.begin(16,2); // wieviel reihen und zeilen das display hat
-  
+  mlcd.begin(16, 2); // wieviel reihen und zeilen das display hat
 
+  // stepper einrichten
+  mStepper01.setMaxSpeed(100);
+  mStepper01.setAcceleration(10);
+  mStepper01.moveTo(-100);
 }
 
-void step(){
-  digitalWrite(pStSt, HIGH);
-  delay(10);
-  digitalWrite(pStSt, LOW);
+void resetStepSet()
+{
+  mStepper01.setMaxSpeed(rand() % 500 + 300);
+  mStepper01.setAcceleration(rand() % 200 + 1);
+  mStepper01.moveTo((rand() % 200 + 1)-100);
+}
+
+void printLCD()
+{
+  mlcd.setCursor(0, 0);
+  mlcd.print("STEP"); // write on lcd
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-  delay(1000);                       // wait for half a second
-  
-  step();
-  mlcd.setCursor(0,0);
-  mlcd.print("STEP"); // write on lcd
-
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  delay(1000);                       // wait for half a second
-  mlcd.setCursor(0,0);
-  mlcd.clear(); // write on lcd
-
+void loop()
+{
+  mStepper01.run();
+  if (!mStepper01.isRunning())
+  {
+    resetStepSet();
+  }
 }
-
